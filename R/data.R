97a99bbcbf3af7df187ded67d41f6f011ed72d03
Simon Jorstedt <jorstedtsimon@gmail.com>
2023-09-13 09:30:01 +0200

    Upload the dataset wiki_graph
    Created the dataset wiki_graph which contains one data frame. This commit also contains associated changes in other files, e.g. documentation.

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Wiki graph
+#' 
+#' @description
+#' A network graph taken from [the Dijkstra algorithms wikipedia page](https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm).
+#' 
+#' @format ## `wiki_graph`
+#' A data frame with 18 rows and 3 columns:
+#' \describe{
+#'    \item{v1}{Start node of an edge in the graph.}
+#'    \item{v2}{End node of an edge in the graph.}
+#'    \item{w}{Weight of the edge.}
+#' }
+#' 
+#' @source <https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm>
+"wiki_graph"
