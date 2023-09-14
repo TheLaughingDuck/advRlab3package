7645355e9d2f8053270d05b6017af9c2cc0c51c7
Simon Jorstedt <jorstedtsimon@gmail.com>
2023-09-14 09:09:58 +0200

    Updated euclidean.R
    Updated euclidean.R with Roxygen skeleton. Created the corresponding .Rd documentation. Created a test file for euclidean.R and moved euclidean.R to the R folder.

--- R/euclidean.R
+++ R/euclidean.R
@@ -1,31 +1,40 @@
+#' Write a Title here
+#'
+#' @description
+#' A short description...
+#'
+#'
+#' @param val1 A numeric value that should be compared with `val1`.
+#' @param val2 A numeric value that should be compared with `val2`.
+#'
+#' @return A numeric: the greatest common divisior of `val1` and `val2`.
+#'
+#' @examples
+#' euclidean(123612, 13892347912)
+#' euclidean(100, 1000)
 euclidean <- function(val1, val2) {
   # Check if both val1 and val2 are non-negative integers
   if (val1 %% 1 != 0 || val2 %% 1 != 0 || val1 < 0 || val2 < 0) {
     stop("Both arguments must be non-negative integers.")
   }
-  
+
   # Ensure that val1 is greater than or equal to val2
   if (val1 < val2) {
     temp_b <- val1
     val1 <- val2
     val2 <- temp_b
-    
+
   }
-  
+
   while (val2 != 0) {
     r <- val1 %% val2
     val1 <- val2
     val2 <- r
-    
+
   }
-  
+
   return(val1)
 }
 
-# Test cases
-euclidean(123612, 13892347912)
-euclidean(100, 1000)
-
-
-result3 <- euclidean(120, 144)
-cat("GCD of 120 and 144:", result3, "\n")            
+#result3 <- euclidean(120, 144)
+#cat("GCD of 120 and 144:", result3, "\n")
---
+++ tests/testthat/test-euclidean.R
@@ -0,0 +1,20 @@
+test_that("GDC is calculated correctly.", {
+  # Test suite from instructions
+  expect_equal(euclidean(123612, 13892347912), 4)
+  expect_equal(euclidean(100, 1000), 100)
+  expect_equal(euclidean(-100, 1000), 100)
+
+  # The authors own tests
+  expect_equal(euclidean(123612, 13892347912), 4)
+  expect_equal(euclidean(100, 1000), 100)
+  expect_equal(euclidean(0, 341), 341)
+  expect_equal(euclidean(1299709, 104729), 1)
+})
+
+
+test_that("Wrong input throws an error.", {
+  expect_error(euclidean("100", 1000))
+  expect_error(euclidean(100, "1000"))
+  expect_error(euclidean(TRUE, "1000"))
+  #expect_error(euclidean(64L, 35L)) #should it be able to work with integers?
+})
